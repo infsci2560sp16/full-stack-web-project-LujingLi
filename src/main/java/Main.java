2df7c87331ad34e6e0dd59f09ff04276267ec030
LujingLi <lujing.li@yahoo.com>
2016-02-18 19:30:56 -0500

    dirty readme.md

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,37 +14,37 @@ import static spark.Spark.get;
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
-        
+
         get("/hello", (req, res) -> "Hello World");
-        
-        get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!!!");
-            
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-        
+
+        // get("/", (request, response) -> {
+        //     Map<String, Object> attributes = new HashMap<>();
+        //     attributes.put("message", "Hello World!!!");
+        //
+        //     return new ModelAndView(attributes, "index.ftl");
+        // }, new FreeMarkerEngine());
+
         get("/db", (req, res) -> {
             Connection connection = null;
             Map<String, Object> attributes = new HashMap<>();
             try {
                 connection = DatabaseUrl.extract().getConnection();
-                
+
                 Statement stmt = connection.createStatement();
                 stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
                 stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
                 ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-                
+
                 ArrayList<String> output = new ArrayList<String>();
                 while (rs.next()) {
                     output.add( "Read from DB: " + rs.getTimestamp("tick"));
                 }
-                
+
                 attributes.put("results", output);
                 return new ModelAndView(attributes, "db.ftl");
             } catch (Exception e) {
@@ -54,7 +54,7 @@ public class Main {
                 if (connection != null) try{connection.close();} catch(SQLException e){}
             }
         }, new FreeMarkerEngine());
-        
+
     }
-    
+
 }
