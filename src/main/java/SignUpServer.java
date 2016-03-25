058b73d348b674962f6d39032baa4fa1b546a34f
LujingLi <lujing.li@yahoo.com>
2016-03-24 20:03:44 -0400

    update

---
+++ src/main/java/SignUpServer.java
@@ -0,0 +1,46 @@
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Date;
+
+import static spark.Spark.get;
+
+
+public class SignUpServer {
+
+    Gson gson = new Gson();
+
+    public SignUpServer() {
+        checkUser();
+    }
+    User user = new User();
+// set default userlist
+    String [] userlist = {"a","b","c","d"};
+
+    private void checkUser() {
+      get("/api/checkuser", (req, res) -> {
+        console.log(req.params)
+        //String username =request
+          // Map<String, Object> data = new HashMap<>();
+          // data.put("title", "Professor");
+          // data.put("name", "Brian");
+          // data.put("description", "INFSCI 2560");
+          // data.put("profession", "Education");
+          //return data;
+      }, gson::toJson);
+
+        get("/api/time/now", (req, res) -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put("currentTime", new Date());
+            return data;
+        }, gson::toJson);
+
+        get("/api/time/now.xml", (req, res) -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put("currentTime", new Date());
+            return data;
+        }, gson::toJson);
+
+
+  }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,18 @@
+public class User {
+	private String username;
+	private String psw;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+
+
+}
