a994ecd41aa7c60daf392e5ae85deacb96d9dc86
LujingLi <lujing.li@yahoo.com>
2016-03-29 03:38:27 -0400

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,14 @@ import java.sql.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Map;
-
-
 import java.net.URI;
-import java.net.URISyntaxException;
+
 
 import static spark.Spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
 import static spark.Spark.get;
-
+import java.net.URISyntaxException;
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
